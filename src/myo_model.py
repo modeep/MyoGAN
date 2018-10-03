1db775b130ef7b307b19fd278fed19ef421df74c
Xsolute Lim <Xsolute01@gmail.com>
2018-10-03 23:43:15 +0900

    MOD: modify myo_model.py code
    print단 Unexpected type ndarray 오류로 인한 수정

--- src/myo_model.py
+++ src/myo_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 from keras.models import Sequential, Model
 from keras.layers import Conv2D, Conv2DTranspose, Reshape, BatchNormalization, Dense, Activation, UpSampling2D, Input
 from keras.layers import Flatten
@@ -34,12 +35,28 @@ Discriminator
 # TODO: 3. Threshold (Activation)
 
 
-class MyoGAN():
+class MyoGAN:
     def __init__(self):
+        self.net_d = self.discriminative()
+        self.net_g = self.generative()
+        fake_image = self.net_g(self.noise_input)
+
+        combined_output = self.net_d(fake_image)
+        self.combined_model = Model(inputs=[self.noise_input], outputs=[combined_output], name='combined')
+
+        self.g_loss_history = []
+        self.d_loss_real_history = []
+        self.d_loss_fake_history = []
+
+        self.d_step = 1
+        self.g_step = 2
+        self.epoch = 30000
+        self.batch_size = 64
         self.noise_size = 100
         self.noise_input = Input(shape=(self.noise_size,))
         self.image_size = 128
         self.image_channel = 1
+
         self.loader = DataLoader_Continous(data_path='./dataset_2018_05_16/',
                                            is_real_image=False,
                                            data_type=2,
@@ -109,3 +126,53 @@ class MyoGAN():
 
         return Model(inputs=inputs, outputs=outputs)
 
+    def sample_generation(self, num, net_g):
+        for _ in range(num):
+            noise = np.random.normal(size=[num, self.noise_size])
+            gan_image = net_g.predict(noise)
+            cv2.imwrite('./model3_output/image/' + 'sample image' + str(_) + '.png', gan_image[_] * 127.5)
+
+        print("generated image")
+
+    def train(self):
+        i = 0
+
+        while i <= self.epoch:
+            # x_train = loader.get_emg_datas(batch_size)
+            images = self.loader.get_images(self.batch_size)
+
+            for _ in range(self.d_step):
+                noise = np.random.normal(size=[self.batch_size, self.noise_size])
+
+                g_z = self.net_g.predict(noise)
+
+                d_loss_real = self.net_d.train_on_batch(images,
+                                                        np.random.uniform(low=0.7, high=1.2, size=self.batch_size))
+                d_loss_fake = self.net_d.train_on_batch(g_z, np.random.uniform(low=0.0, high=0.3, size=self.batch_size))
+
+                self.d_loss_real_history.append(d_loss_real)
+                self.d_loss_fake_history.append(d_loss_fake)
+                self.d_loss = np.sum([d_loss_fake, d_loss_real])
+
+            for _ in range(self.g_step):
+                noise = np.random.normal(size=[self.batch_size, self.noise_size])
+                combined_loss = self.combined_model.train_on_batch(noise, np.random.uniform(low=0.7, high=1.2,
+                                                                                            size=self.batch_size))
+
+                self.g_loss_history.append(combined_loss)
+
+            print("%d [D loss real: %f] [D loss fake: %f] [G loss: %f]" % (
+                i, self.d_loss_real_history[-1], self.d_loss_fake_history[-1], self.g_loss_history[-1]))
+
+            # print("%d [D loss real: %f] [D loss fake: %f] [D loss: %f] [G loss: %f]" % (
+            #     i, self.d_loss_real_history[-1], self.d_loss_fake_history[-1], self.d_loss, self.g_loss_history[-1]))
+
+            if i % 500 == 0:
+                gan_image = self.net_g.predict(np.random.normal(size=[self.batch_size, self.noise_size]))
+                print("gan image2 : ", gan_image[0].shape)
+                cv2.imwrite('./model3_output/image/' + 'fake_image' + str(i) + '.png', gan_image[0] * 127.5)
+                # cv2.imwrite('./output_image3/' + 'real_image'+ str(i) + '.png', images[0] * 127.5)
+
+            i += 1
+
+        self.sample_generation(32, self.net_g)
